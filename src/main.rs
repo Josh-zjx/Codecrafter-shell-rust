74b96795c4ea192ddfa04f256d15e4628ab7329f
Jiaxu Zhu <josh.zhujiaxu@gmail.com>
2024-05-29 16:34:06 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::process::exit;
 //use std::io::prelude::*;
 #[allow(unused_imports)]
 use std::io::{self, Write};
@@ -14,9 +15,33 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
     while stdin.read_line(&mut input).is_ok() {
-        println!("{}: command not found", input.strip_suffix("\n").unwrap());
+        let command = parse_input(&input);
+        match command {
+            Command::ERROR(name) => {
+                println!("{}: command not found", name);
+            }
+            Command::EXIT(val) => exit(val),
+            _default => {}
+        }
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
     }
+}
+
+#[derive(Debug, Clone)]
+pub enum Command<'a> {
+    EXIT(i32),
+    ECHO(&'a str),
+    ERROR(&'a str),
+}
+
+fn parse_input(input: &str) -> Command {
+    let input = input.strip_suffix('\n').unwrap();
+    let input_group: Vec<_> = input.split(' ').collect();
+    match *input_group.first().unwrap() {
+        "exit" => Command::EXIT(input_group.get(1).unwrap().parse::<i32>().unwrap()),
+        _default => Command::ERROR(input),
+    }
+    //Command::ERROR(input)
 }
