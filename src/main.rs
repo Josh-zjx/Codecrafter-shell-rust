f835e539e309887dd4bade0bcf26495a1a54f822
Jiaxu Zhu <josh.zhujiaxu@gmail.com>
2026-03-31 18:39:46 +0800

    implement quote handler and redirection

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::path::{Path, PathBuf};
 use std::process::exit;
 use std::process::Command;
 use std::{env, fs};
+use std::os::unix::fs::PermissionsExt;
 //use std::io::prelude::*;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+
 
 fn main() {
     let stdin = io::stdin();
@@ -13,29 +15,71 @@ fn main() {
     io::stdout().flush().unwrap();
     while stdin.read_line(&mut input).is_ok() {
         let input_string = input.strip_suffix('\n').unwrap();
+
         if let Some(command) = parse_input(input_string) {
             match command {
-                ShellCommand::EXIT(val) => exit(val),
-                ShellCommand::ECHO(argument) => {
-                    println!("{}", argument);
+                ShellCommand::Empty() => {
+                    // NOTE: Empty command, just do nothing
+                 },
+                ShellCommand::Exit(val) => exit(val),
+                ShellCommand::Echo(argument, _rout, _rerr, _rout_append, _rerr_append) => {
+                    if _rout != "" {
+                        // NOTE: stdout redirection
+                        let mut f = if _rout_append {
+                            fs::OpenOptions::new().append(true).create(true).open(_rout).expect("Unable to open file")
+                        } else {
+                            fs::OpenOptions::new().write(true).create(true).open(_rout).expect("Unable to open file")
+                        };
+
+                        f.write_all((argument.join(" ") + "\n").as_bytes()).expect("Unable to write to file");
+                    } else {
+                        println!("{}", argument.join(" "));
+                    }
+                    if _rerr != "" {
+                        // NOTE: stdout redirection
+                        fs::write(_rerr, "").expect("Unable to write to file");
+                    }
                 }
-                ShellCommand::TYPE(argument) => handle_command_type(argument),
-                ShellCommand::PWD() => {
+                ShellCommand::Type(argument) => handle_command_type(argument.as_str()),
+                ShellCommand::Pwd() => {
                     println!("{}", std::env::current_dir().unwrap().to_str().unwrap())
                 }
-                ShellCommand::CD(argument) => handle_command_cd(argument),
-                ShellCommand::Program((command, arguments)) => {
-                    let command_type = type_of_command(command);
+                ShellCommand::Cd(argument) => handle_command_cd(argument.as_str()),
+                ShellCommand::Program(command, arguments, _rout, _rerr, _rout_append, _rerr_append) => {
+                    let command_type = type_of_command(command.as_str());
                     match command_type {
                         CommandType::Nonexistent => {
                             println!("{}: command not found", input_string);
                         }
-                        CommandType::Program(path) => {
-                            let output = Command::new(path)
-                                .args(arguments.split(' '))
+                        CommandType::Program(_path) => {
+                            let output = Command::new(command)
+                                .args(arguments)
                                 .output()
                                 .expect("fail to run program");
-                            print!("{}", String::from_utf8_lossy(&output.stdout))
+                            if _rout != "" {
+                                // NOTE: stdout redirection
+                                let mut f = if _rout_append {
+                                    fs::OpenOptions::new().append(true).create(true).open(_rout).expect("Unable to open file")
+                                } else {
+                                    fs::OpenOptions::new().write(true).create(true).open(_rout).expect("Unable to open file")
+                                };
+
+                                f.write_all(&output.stdout).expect("Unable to write to file");
+                            } else {
+                                print!("{}", String::from_utf8_lossy(&output.stdout));
+                            }
+                            if _rerr != "" {
+                                // NOTE: stderr redirection
+                                let mut f = if _rerr_append {
+                                    fs::OpenOptions::new().append(true).create(true).open(_rerr).expect("Unable to open file")
+                                } else {
+                                    fs::OpenOptions::new().write(true).create(true).open(_rerr).expect("Unable to open file")
+                                };
+
+                                f.write_all(&output.stderr).expect("Unable to write to file");
+                            } else {
+                                print!("{}", String::from_utf8_lossy(&output.stderr));
+                            }
                         }
                         CommandType::Builtin => {}
                     };
@@ -75,28 +119,170 @@ fn handle_command_cd(argument: &str) {
 }
 
 #[derive(Debug, Clone)]
-pub enum ShellCommand<'a> {
-    EXIT(i32),
-    ECHO(&'a str),
-    CD(&'a str),
-    TYPE(&'a str), //ERROR(&'a str),
-    PWD(),
-    Program((&'a str, &'a str)),
+pub enum ShellCommand {
+    Empty(),
+    Exit(i32),
+    Echo(Vec<String>, String, String, bool, bool), // (arguments, stdout, stderr, stdout_append, stderr_append)
+    Cd(String), // (path)
+    Type(String), //ERROR(&'a str),
+    Pwd(),
+    Program(String, Vec<String>, String, String, bool, bool), // (command, arguments, stdout, stderr, stdout_append, stderr_append)
 }
 
 fn parse_input(input: &str) -> Option<ShellCommand> {
-    let (command, arguments) = match input.find(' ') {
-        Some(_index) => input.split_once(' ')?,
-        None => (input, ""),
+    // NOTE: Only space and tab are considered as delimiters right now, other operators might be appended later
+
+    // NOTE: split_once isn't good enough. Need a full tokenizer to split input into strings
+    // NOTE: It is OK to do all tokenization here
+    let mut single_quote_open = false;
+    let mut double_quote_open = false;
+    let mut escape_next = false;
+    let mut parsed_input: Vec<String> = vec![];
+    let mut current_string:String = "".to_string();
+    for c in input.chars() {
+        if escape_next {
+            // FIXME: Double quote escape is limited but not really handled here
+            current_string += &c.to_string();
+            escape_next = false;
+        } else if c == '\'' && !double_quote_open {
+            single_quote_open = !single_quote_open;
+        } else if c == '\\' && !single_quote_open {
+            escape_next = true;
+        } else if c == '"' && !single_quote_open {
+            double_quote_open = !double_quote_open;
+        } else if (c == ' ' || c == '\t')  && !single_quote_open && !double_quote_open {
+            // FIXME: the union of matching delimiters feels ugly and nonextensible, but it is OK for now
+            if !current_string.is_empty() {
+                parsed_input.push(current_string.clone());
+                current_string.clear();
+            }
+        } else {
+            current_string += &c.to_string();
+        }
+    }
+    if !current_string.is_empty() {
+        parsed_input.push(current_string);
+    }
+
+    
+    let (command, mut arguments) = if parsed_input.len() > 1 {
+        (parsed_input[0].clone(), parsed_input[1..].to_vec())
+    } else if parsed_input.len() == 1 {
+        (parsed_input[0].clone(), Vec::new())
+    } else {
+        ("".to_string(), Vec::new())
     };
 
-    match command {
-        "exit" => Some(ShellCommand::EXIT(arguments.parse::<i32>().unwrap())),
-        "echo" => Some(ShellCommand::ECHO(arguments)),
-        "type" => Some(ShellCommand::TYPE(arguments)),
-        "pwd" => Some(ShellCommand::PWD()),
-        "cd" => Some(ShellCommand::CD(arguments)),
-        _default => Some(ShellCommand::Program((command, arguments))),
+    match command.as_str() {
+        "exit" => Some(ShellCommand::Exit(arguments.get(0).unwrap_or(&"0".to_string()).parse::<i32>().unwrap_or(0))),
+        "echo" => { 
+            let mut rout:String = "".to_string();
+            let mut rout_append = false;
+            let mut rout_i = 0;
+            for (i, word) in arguments.iter().enumerate() {
+                if word == "1>" || word == ">" {
+                    // NOTE: stdout redirection
+                    if arguments.len() > i + 1 {
+                        rout = arguments[i + 1].clone();
+                        rout_i = i;
+                        break;
+                    }
+                }else if word == "1>>" || word == ">>" {
+                    if arguments.len() > i + 1 {
+                        rout = arguments[i + 1].clone();
+                        rout_i = i;
+                        rout_append = true;
+                        break;
+                    }
+                }
+            }
+            if rout != "" {
+                arguments.remove(rout_i + 1);
+                arguments.remove(rout_i);
+            }
+            let mut rerr_append = false;
+            let mut rerr:String = "".to_string();
+            let mut rerr_i = 0;
+            for (i, word) in arguments.iter().enumerate() {
+                if word == "2>" {
+                    // NOTE: stderr redirection
+                    if arguments.len() > i + 1 {
+                        rerr = arguments[i + 1].clone();
+                        rerr_i = i;
+                        break;
+                    }
+                } else if word == "2>>" {
+                    // NOTE: stderr redirection
+                    if arguments.len() > i + 1 {
+                        rerr = arguments[i + 1].clone();
+                        rerr_i = i;
+                        rerr_append = true;
+                        break;
+                    }
+                }
+            }
+            if rerr != "" {
+                arguments.remove(rerr_i + 1);
+                arguments.remove(rerr_i);
+            }
+            Some(ShellCommand::Echo(arguments, rout, rerr, rout_append, rerr_append))
+         },
+        "type" => Some(ShellCommand::Type(arguments.get(0).unwrap_or(&"".to_string()).to_string())),
+        "pwd" => Some(ShellCommand::Pwd()),
+        "cd" => Some(ShellCommand::Cd(arguments.get(0).unwrap_or(&"".to_string()).to_string())),
+        "" => Some(ShellCommand::Empty()),
+        _default => { 
+            let mut rout:String = "".to_string();
+            let mut rout_append = false;
+            let mut rout_i = 0;
+            for (i, word) in arguments.iter().enumerate() {
+                if word == "1>" || word == ">" {
+                    // NOTE: stdout redirection
+                    if arguments.len() > i + 1 {
+                        rout = arguments[i + 1].clone();
+                        rout_i = i;
+                        break;
+                    }
+                }else if word == "1>>" || word == ">>" {
+                    if arguments.len() > i + 1 {
+                        rout = arguments[i + 1].clone();
+                        rout_i = i;
+                        rout_append = true;
+                        break;
+                    }
+                }
+            }
+            if rout != "" {
+                arguments.remove(rout_i + 1);
+                arguments.remove(rout_i);
+            }
+            let mut rerr_append = false;
+            let mut rerr:String = "".to_string();
+            let mut rerr_i = 0;
+            for (i, word) in arguments.iter().enumerate() {
+                if word == "2>" {
+                    // NOTE: stderr redirection
+                    if arguments.len() > i + 1 {
+                        rerr = arguments[i + 1].clone();
+                        rerr_i = i;
+                        break;
+                    }
+                } else if word == "2>>" {
+                    // NOTE: stderr redirection
+                    if arguments.len() > i + 1 {
+                        rerr = arguments[i + 1].clone();
+                        rerr_i = i;
+                        rerr_append = true;
+                        break;
+                    }
+                }
+            }
+            if rerr != "" {
+                arguments.remove(rerr_i + 1);
+                arguments.remove(rerr_i);
+            }
+            Some(ShellCommand::Program(command, arguments, rout, rerr, rout_append, rerr_append)) 
+        },
     }
 }
 
@@ -124,8 +310,14 @@ fn type_of_command(command: &str) -> CommandType {
                         Err(_err) => continue,
                     };
                     for item in folder.into_iter() {
-                        if item.as_ref().unwrap().file_name() == command {
-                            return CommandType::Program(item.unwrap().path());
+                        if item.as_ref().unwrap().file_name() == command{
+                            let metadata = match fs::metadata(item.as_ref().unwrap().path()){
+                                Ok(meta) => meta,
+                                Err(_err) => continue,
+                            };
+                            if metadata.is_file() && metadata.permissions().mode() & 0o111 != 0 {
+                                return CommandType::Program(item.unwrap().path());
+                            }
                         }
                     }
                 }
